bf1cbea09842b7c38ac6cc7e76254df87a651b0d
NatoubiZineb <natoubizineb17@gmail.com>
2024-04-30 10:45:31 +0100

    swagger 2

--- src/main/java/org/gmaoback/gmaomondi_backend/GmaoMondiBackendApplication.java
+++ src/main/java/org/gmaoback/gmaomondi_backend/GmaoMondiBackendApplication.java
@@ -2,14 +2,13 @@ package org.gmaoback.gmaomondi_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
+//@EnableOpenApi
 public class GmaoMondiBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GmaoMondiBackendApplication.class, args);
 	}
-
 }
