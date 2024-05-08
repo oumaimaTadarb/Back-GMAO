f76fe5f6e8047f60dfcc71d0827afb82bd4815ab
ZainebBenchaaboune <benchaabounezaineb@gmail.com>
2024-05-08 16:15:19 +0100

    fr

--- src/main/java/org/gmaoback/gmaomondi_backend/controllers/FournisseurController.java
+++ src/main/java/org/gmaoback/gmaomondi_backend/controllers/FournisseurController.java
@@ -1,7 +1,10 @@
 package org.gmaoback.gmaomondi_backend.controllers;
 import org.gmaoback.gmaomondi_backend.dao.entities.Fournisseur;
+import org.gmaoback.gmaomondi_backend.dto.FournisseurDTO;
 import org.gmaoback.gmaomondi_backend.services.FournisseurService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +15,80 @@ public class FournisseurController {
 
     @Autowired
     private FournisseurService fournisseurService;
+
+    @PostMapping
+    public ResponseEntity<Fournisseur> addFournisseur(@RequestBody FournisseurDTO fournisseurDto) {
+        Fournisseur savedFournisseur = fournisseurService.addNewFournisseur(fournisseurDto);
+        return new ResponseEntity<>(savedFournisseur, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fournisseur> updateFournisseur(@PathVariable Long id, @RequestBody FournisseurDTO fournisseurDTO) {
+        Fournisseur updatedFournisseur = fournisseurService.updateFournisseur(id, fournisseurDTO);
+        if (updatedFournisseur != null) {
+            return new ResponseEntity<>(updatedFournisseur, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFournisseur(@PathVariable Long id) {
+        try {
+            fournisseurService.deleteFournisseur(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FournisseurDTO>> listFournisseursDTO() {
+        List<FournisseurDTO> fournisseurs = fournisseurService.listFournisseursDTO();
+        return new ResponseEntity<>(fournisseurs, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Fournisseur>> listFournisseurs() {
+        List<Fournisseur> fournisseurs = fournisseurService.listFournisseurs();
+        return new ResponseEntity<>(fournisseurs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fournisseur> loadFournisseurById(@PathVariable Long id) {
+        Fournisseur fournisseur = fournisseurService.loadFournisseurById(id);
+        if (fournisseur != null) {
+            return new ResponseEntity<>(fournisseur, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PatchMapping("/updateData/{id}")
+    public ResponseEntity<Fournisseur> updateFournisseurData(@PathVariable Long id, @RequestBody FournisseurDTO fournisseurDTO) {
+        Fournisseur existingFournisseur = fournisseurService.loadFournisseurById(id);
+        if (existingFournisseur == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Fournisseur updatedFournisseur = fournisseurService.updateData(existingFournisseur, fournisseurDTO);
+        return new ResponseEntity<>(updatedFournisseur, HttpStatus.OK);
+    }
+    @GetMapping("/fournisseurDTO/{id}")
+    public ResponseEntity<FournisseurDTO> loadFournisseurByFournisseurId(@PathVariable Long id) {
+        FournisseurDTO fournisseurDto = fournisseurService.loadFournisseurByFournisseurId(id);
+        if (fournisseurDto != null) {
+            return new ResponseEntity<>(fournisseurDto, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/codeSapFr/{codeSapFr}")
+    public ResponseEntity<Fournisseur> loadFournisseurByCodeSapFr(@PathVariable Long codeSapFr) {
+        Fournisseur fournisseur = fournisseurService.loadFournisseurBycodeSapFr(codeSapFr);
+        if (fournisseur != null) {
+            return new ResponseEntity<>(fournisseur, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
 //    @PostMapping("/saveFournisseur")
 //    public Fournisseur saveFournisseur(@RequestBody Fournisseur fournisseur) {
 //        return fournisseurService.saveFournisseur(fournisseur);
@@ -50,4 +127,4 @@ public class FournisseurController {
 //    public List<Fournisseur> getAllFournisseurs() {
 //        return fournisseurService.getAllFournisseurs();
 //    }
-}
+
--- src/main/java/org/gmaoback/gmaomondi_backend/services/FournisseurService.java
+++ src/main/java/org/gmaoback/gmaomondi_backend/services/FournisseurService.java
@@ -1,16 +1,33 @@
 package org.gmaoback.gmaomondi_backend.services;
 
 import org.gmaoback.gmaomondi_backend.dao.entities.Fournisseur;
+import org.gmaoback.gmaomondi_backend.dto.FournisseurDTO;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface FournisseurService {
-    Fournisseur getFournisseurBycodeSapFr(Long codeSapFr);
-    Fournisseur updateFournisseurByCodeSapFr(Long codeSapFr, Fournisseur updatedFournisseur);
-    void deleteFournisseurByCodeSapFr(Long codeSapFr);
-    Fournisseur saveFournisseur(Fournisseur fournisseur);
-    Fournisseur updateFournisseur(Fournisseur fournisseur);
-    Fournisseur getFournisseurById(Long idFournisseur);
-    void deleteFournisseurById(Long idFournisseur);
-    List<Fournisseur> getAllFournisseurs();
+
+    Fournisseur addNewFournisseur(FournisseurDTO fournisseurDto);
+    Fournisseur updateFournisseur(Long id, FournisseurDTO fournisseurDTO);
+    Fournisseur updateData(Fournisseur fournisseur, FournisseurDTO fournisseurDto);
+    void deleteFournisseur(Long id);
+    List<FournisseurDTO> listFournisseursDTO();
+    List<Fournisseur> listFournisseurs();
+    FournisseurDTO loadFournisseurByFournisseurId(Long id);
+    Fournisseur loadFournisseurById(Long id);
+    Fournisseur loadFournisseurBycodeSapFr(Long codeSapFr);
+
+
+
+
+//    Fournisseur getFournisseurBycodeSapFr(Long codeSapFr);
+//    Fournisseur updateFournisseurByCodeSapFr(Long codeSapFr, Fournisseur updatedFournisseur);
+//    void deleteFournisseurByCodeSapFr(Long codeSapFr);
+//    Fournisseur saveFournisseur(Fournisseur fournisseur);
+//    Fournisseur updateFournisseur(Fournisseur fournisseur);
+//    Fournisseur getFournisseurById(Long idFournisseur);
+//    void deleteFournisseurById(Long idFournisseur);
+//    List<Fournisseur> getAllFournisseurs();
 }
