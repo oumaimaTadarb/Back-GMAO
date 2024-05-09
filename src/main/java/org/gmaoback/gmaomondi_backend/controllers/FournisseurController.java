1e7f47ac90924593eb14de2175235dc5c390b55f
NatoubiZineb <natoubizineb17@gmail.com>
2024-05-09 15:01:42 +0100

    cross Origin

--- src/main/java/org/gmaoback/gmaomondi_backend/controllers/FournisseurController.java
+++ src/main/java/org/gmaoback/gmaomondi_backend/controllers/FournisseurController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/fournisseurs")
+@CrossOrigin("*")
 public class FournisseurController {
 
     private final FournisseurService fournisseurService;
